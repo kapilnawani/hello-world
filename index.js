c1900995f2769e1b1deaf7375303abb57feab74b
kapilnawani <4119388+kapilnawani@users.noreply.github.com>
2019-06-25 18:48:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('XGAMER'));
 app.listen(process.env.PORT || 5000);
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`))
