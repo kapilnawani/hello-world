6c2b47ae3ad15d502cddaee35e2f6278a091207e
kapilnawani <4119388+kapilnawani@users.noreply.github.com>
2019-06-25 17:27:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
      
     var server = http.createServer(function handler(req, res) {
         //res.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
+        //    'Content-Type': 'text/plain'
+        //});
         res.end('Helloooooooooooo World!!!');
     });
      
