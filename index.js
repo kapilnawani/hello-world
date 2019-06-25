cab76e37a6deba3ff0f78b1e4ca0412d7bf92058
kapilnawani <kapilnawani@gmail.com>
2019-06-25 17:04:34 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    var http = require('http');
+     
+    var server = http.createServer(function handler(req, res) {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Hello World!!!');
+    });
+     
+    server.listen(9000);
+    console.log('Server running at http://127.0.0.1:9000/');
